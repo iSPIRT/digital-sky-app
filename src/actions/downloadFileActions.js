821a32881d7dcbc251b60723bfd2770d4dcdee0d
Praveen Shivanagoudar <praveens@Praveens-MacBook-Pro.local>
2018-07-24 12:04:37 +0530

    File Download

---
+++ src/actions/downloadFileActions.js
@@ -0,0 +1,21 @@
+import { fileDownloadService } from '../services/fileDownloadService';
+
+import fileDownload from 'js-file-download';
+
+export const downloadFile = (path, fileName) => {
+
+    return dispatch => {
+        fileDownloadService.download(path)
+                    .then(
+                        data => {
+                           fileDownload(data, fileName);
+                        },
+                        errors => {
+                            console.log("Error downloading file:"+path)
+                        }
+                    );
+
+    };
+
+}
+
