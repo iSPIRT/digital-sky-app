379900a1f2fa03075f42db422de53430a9cd3476
praveens <praveens>
2018-07-28 12:20:10 +0530

    User Dashboard Page With Links

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -10,34 +10,67 @@ class Dashboard extends React.Component {
     const individualOperatorProfileId = localStorage.getItem('individualOperatorProfileId');
     const organizationOperatorProfileId = localStorage.getItem('organizationOperatorProfileId');
 
-    const hasProfile = ( pilotProfileId > 0) || (individualOperatorProfileId > 0) ||(organizationOperatorProfileId > 0)
+    const hasOperatorProfile = (individualOperatorProfileId > 0) || (organizationOperatorProfileId > 0)
+    const hasPilotProfile = ( pilotProfileId > 0)
 
     return (
-       <div class="page-dashboard">
-            { !hasProfile &&
-                    <section id="profile-status">
+            <div>
+                <div class="page-header dashboard-header">
+                    <div class="grid-container">
+                        <div class="grid-x grid-padding-x">
+                            <div class="large-12 cell">
+                                {   !hasPilotProfile &&
+                                    <div>
+                                        <p> If you are pilot, setup your pilot profile to begin</p>
+                                        <Link to="/profile" class="button">Setup my Pilot Profile</Link>
+                                    </div>
+                                }
+
+                                {   hasPilotProfile &&
+                                    <Link to="/profile" class="button">Update my Pilot Profile</Link>
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br/>
+                <br/>
+                <div class="page-header dashboard-header">
+                    <div class="grid-container">
+                        <div class="grid-x grid-padding-x">
+                            <div class="large-12 cell">
+                                {   !hasOperatorProfile &&
+                                    <div>
+                                        <p> If you are operator, setup your operator profile to begin</p>
+                                        <Link to="/profile" class="button">Setup my Operator Profile</Link>
+                                    </div>
+                                }
+
+                                {   hasOperatorProfile &&
+                                    <div>
+                                        <Link to="/profile" class="button">Update my Operator Profile</Link>
+                                        <Link to="/localDroneAcquisitionApplication" class="button">Apply here for Local Drone Acquisition</Link>
+                                        <Link to="/importDroneApplication" class="button">Apply here to Import Drones</Link>
+                                        <Link to="/uaopApplication" class="button">Apply here to for UAOP Licence</Link>
+                                        <Link to="/uinApplication" class="button">Apply here to for UIN</Link>
+                                    </div>
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="page-dashboard">
+                    <section id="application-status">
                         <div class="grid-container">
                             <div class="grid-x grid-padding-x">
                                 <div class="large-12 cell">
-                                    <br/>
-                                    <br/>
-                                    <p> Looks like you have not setup your profile</p>
-                                    <Link to="/profile" class="button">Setup my Profile</Link>
+                                    <p> Application Status</p>
                                 </div>
                             </div>
                         </div>
                     </section>
-            }
-            <section id="application-status">
-                <div class="grid-container">
-                    <div class="grid-x grid-padding-x">
-                        <div class="large-12 cell">
-                            <p> My applications</p>
-                        </div>
-                    </div>
                 </div>
-            </section>
-        </div>
+            </div>
     );
   }
 }
