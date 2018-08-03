ee6e0e963ffa4011e3036d57d77b93ca14e88fd9
praveens <praveens>
2018-08-03 19:27:48 +0530

    Account verification

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -4,6 +4,9 @@ export const LOAD_USER_APPLICATIONS_REQUEST = "LOAD_USER_APPLICATIONS_REQUEST";
 export const LOAD_USER_APPLICATIONS_SUCCESS = "LOAD_USER_APPLICATIONS_SUCCESS";
 export const LOAD_USER_APPLICATIONS_FAILURE = "LOAD_USER_APPLICATIONS_FAILURE";
 
+export const VERIFY_ACCOUNT_SUCCESS = "VERIFY_ACCOUNT_SUCCESS";
+export const VERIFY_ACCOUNT_FAILURE = "VERIFY_ACCOUNT_FAILURE";
+
 export const loadApplicationsAction = () => {
   return dispatch => {
     dispatch(request());
@@ -25,5 +28,25 @@ export const loadApplicationsAction = () => {
   }
   function failure(errors) {
     return { type: LOAD_USER_APPLICATIONS_FAILURE, errors };
+  }
+};
+
+export const verifyAccountAction = (token) => {
+  return dispatch => {
+    userService.verifyAccount(token).then(
+      data => {
+        dispatch(success());
+      },
+      errors => {
+        dispatch(failure(errors));
+      }
+    );
+  };
+
+  function success() {
+    return { type: VERIFY_ACCOUNT_SUCCESS};
+  }
+  function failure(errors) {
+    return { type: VERIFY_ACCOUNT_FAILURE, errors };
   }
 };
