2f0e3e09cc349bd8687716c173a4849dad00013d
charan1994 <charana@sahajsoft.com>
2018-11-21 14:57:03 +0530

    making press release inline for multiple articles

--- src/components/HomeSectionPressRelease.jsx
+++ src/components/HomeSectionPressRelease.jsx
@@ -10,7 +10,7 @@ class HomeSectionPressRelease extends React.Component {
                 <div className="grid-x grid-padding-x">
                     <div className="large-12 cell">
                         <h2>Press Releases</h2>
-                        <div className="article">
+                        <div className="article large-6">
                             <a href="http://pib.nic.in/newsite/PrintRelease.aspx?relid=183093" target="_blank"><h4>Government announces Regulations for Drones</h4></a>
                             <div className="date">27 August 2018</div>
                         </div>
