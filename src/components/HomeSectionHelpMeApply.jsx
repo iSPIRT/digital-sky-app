94bf52fad693cda11cecf9363224c65168b38c17
charan1994 <charana@sahajsoft.com>
2018-12-01 12:16:34 +0530

    fixing the help me apply

--- src/components/HomeSectionHelpMeApply.jsx
+++ src/components/HomeSectionHelpMeApply.jsx
@@ -12,10 +12,12 @@ class HomeSectionHelpMeApply extends React.Component {
             showAnswerUnder: false,
             showAnswerOver: false,
             showAnswerPilot: false,
+            showMicroDrone: false,
+            showSmallDrone: false,
+            showBuildingIndia: false,
+            showImport:false,
             showAnswerOperator: false,
-            showAnswerManufacturer: false,
-            showAnswerInIndia: false,
-            showAnswerImport: false
+            showAnswerManufacturer: false
         };
     }
 
@@ -29,8 +31,10 @@ class HomeSectionHelpMeApply extends React.Component {
                     showAnswerPilot: false,
                     showAnswerOperator: false,
                     showAnswerManufacturer: false,
-                    showAnswerInIndia: false,
-                    showAnswerImport: false
+                    showMicroDrone: false,
+                    showSmallDrone: false,
+                    showBuildingIndia: false,
+                    showImport:false
                 });
             }
             else if(event.target.value === 'over'){
@@ -40,12 +44,35 @@ class HomeSectionHelpMeApply extends React.Component {
                     showAnswerPilot: false,
                     showAnswerOperator: false,
                     showAnswerManufacturer: false,
-                    showAnswerInIndia: false,
-                    showAnswerImport: false
+                    showMicroDrone: false,
+                    showSmallDrone: false,
+                    showBuildingIndia: false,
+                    showImport:false
                 });
             }
         }
         else if(event.target.name === 'selectLicenseType'){
+            if(event.target.value==='manufacturer'){
+                this.setState({
+                    showSmallDrone: false,
+                    showMicroDrone: false,
+                    showOperatorAnswerImport: false,
+                    showOperatorAnswerInIndia: false})
+            }
+            else if(event.target.value==='operator'){
+                this.setState({
+                    showMicroDrone: false,
+                    showSmallDrone: false,
+                    showBuildingIndia: false,
+                    showImport: false})
+            }
+            else {
+                this.setState({
+                    showOperatorAnswerImport: false,
+                    showOperatorAnswerInIndia: false,
+                    showBuildingIndia: false,
+                    showImport: false})
+            }
             this.setState({
                 showAnswerPilot: event.target.value==='pilot',
                 showAnswerOperator: event.target.value==='operator',
@@ -53,11 +80,23 @@ class HomeSectionHelpMeApply extends React.Component {
             });
         }
 
-        else if(event.target.name === 'selectAcquisitionType'){
+        else if(event.target.name === 'selectAcquisitionTypeOperator'){
             this.setState({
-                showAnswerInIndia: event.target.value==='in-india',
-                showAnswerImport: event.target.value==='import'
+                showOperatorAnswerInIndia: event.target.value==='in-india',
+                showOperatorAnswerImport: event.target.value==='import'                
             });
+        }
+        else if(event.target.name === 'selectAcquisitionTypePilot'){
+            this.setState({
+                showMicroDrone: event.target.value==='micro',
+                showSmallDrone: event.target.value==='small'                
+            })
+        }
+        else if(event.target.name === 'selectAcquisitionTypeManufacturer'){
+            this.setState({
+                showBuildingIndia: event.target.value==='in-india',
+                showImport: event.target.value==='import'                
+            })
         }
     }
 
@@ -69,8 +108,12 @@ class HomeSectionHelpMeApply extends React.Component {
             showAnswerPilot,
             showAnswerOperator,
             showAnswerManufacturer,
-            showAnswerInIndia,
-            showAnswerImport
+            showMicroDrone,
+            showSmallDrone,
+            showBuildingIndia,
+            showOperatorAnswerImport,
+            showOperatorAnswerInIndia,
+            showImport
         } = this.state;
 
         return (
@@ -97,44 +140,62 @@ class HomeSectionHelpMeApply extends React.Component {
                                       <option value="manufacturer">manufacturer</option>
                                     </select>
                                     and
-                                    <select className="acquisition_type" name="selectAcquisitionType" onChange={this.handleChange}>
-                                      <option disabled selected value="">Select</option>
-                                      <option value="in-india">purchasing a drone in India</option>
-                                      <option value="import">imorting a drone in India</option>
-                                    </select>
+                                    {
+                                        showAnswerOperator && <select className="acquisition_type" name="selectAcquisitionTypeOperator" onChange={this.handleChange}>
+                                            <option disabled selected value="">Select</option>
+                                            <option value="in-india">purchasing a drone in India</option>
+                                            <option value="import">imorting a drone in India</option>
+                                        </select>
+                                    }
+                                    {
+                                        showAnswerPilot && <select className="acquisition_type" name="selectAcquisitionTypePilot" onChange={this.handleChange}>
+                                            <option disabled selected value="">Select</option>
+                                            <option value="micro">I'm flying a micro drone</option>
+                                            <option value="small">I'm flying a small or above drone</option>
+                                        </select>
+                                    }
+                                    {
+                                        showAnswerManufacturer && <select className="acquisition_type" name="selectAcquisitionTypeManufacturer" onChange={this.handleChange}>
+                                            <option disabled selected value="">Select</option>
+                                            <option value="in-india">I'm building a drone in India</option>
+                                            <option value="import">I'm importing a drone</option>
+                                        </select>
+                                    }
                                 </span>
                                 }
                             </p>
                             <div className="answer">
                                 <p className={showAnswerUnder ? '' : 'under'} >
                                 <strong>Answer:</strong>
-                                Nano drones are currently exempt from registration, import licenses as well as NPNT compliance. You can fly your drone up to 50 ft and in enclosed premises, today! Please do not forget to read our <a href="http://dgca.gov.in/cars/RPAS-Do's%20and%20Don'ts.pdf" target="_blank"><font color="FFFFFF">Do’s and Dont's</font></a></p>
-                                <p className={showAnswerOver ? '' : 'over'} ><strong>Answer:</strong> First you need to create an account here on DroneStack.</p>
+                                Nano drones are currently exempt from registration, import licenses as well as NPNT compliance. You can fly your drone up to 50 ft and in enclosed premises, today! Please do not forget to read our <a href="http://dgca.gov.in/cars/RPAS-Do's%20and%20Don'ts.pdf" target="_blank"><font color="FFFFFF">Do’s and Don'ts</font></a></p>
 
                                 <p className={showAnswerPilot ? '' : 'pilot'} >
                                 As the pilot of a drone, you must be well-versed with the CAR and the Do’s and Don’ts to understand what is required of you. You are primarily responsible for safe flight operations and please be aware of people, structures and entities in your flight plan that you could affect. Please register yourself, to get a Pilot ID that will be required for permission to fly in NPNT complaint drones.<br></br>
-                                Do follow guidelines as per section 12 of the CAR, and avoid areas as per Section 13 of the CAR. <br></br>
-                                (I’m flying Small or above drone) You are also required to get remote pilot training as per Section 9 of the CAR from an approved FTO.
+                                </p>
+                                <p className={showMicroDrone ? '' : 'pilot'} >
+                                Do follow guidelines as per section 12 of the CAR, and avoid areas as per Section 13 of the CAR.<br></br>
+                                </p>
+                                <p className={showSmallDrone ? '' : 'pilot'} >
+                                You are also required to get remote pilot training as per Section 9 of the CAR from an approved FTO.
                                 </p>
                                 <p className={showAnswerOperator ? '' : 'operator'} >
                                 As an owner of a drone, you must be well-versed with the CAR and the Do’s and Don’ts to understand what is required of you. Unless exempted by section 7.2 of the CAR, you will need a UAOP. Refer to Section 7 of the CAR for documentation required.<br></br>
+                                </p>
+                                <p className={showOperatorAnswerInIndia ? '' : 'operator'} >
                                 Please ensure that the drone you are purchasing is an NPNT compliant drone. You will only be able to generate UINs for NPNT Compliant drone.<br></br>
-                                (I’m importing a drone) For imports, a clearance from DGFT is also required along with NPNT compliance. If the drone is not a registered type in Digital Sky, you will also need to register as a manufacturer.<br></br>
-                                After creation of your profile, please pay the fees on Bharat Kosh. Upload the payment receipt and necessary documentation. Your approval will take up to 7 days to process.
+                                </p>
+                                <p className={showOperatorAnswerImport ? '' : 'operator'} >
+                                For imports, a clearance from DGFT is also required along with NPNT compliance. If the drone is not a registered type in Digital Sky, you will also need to register as a manufacturer.<br></br>
                                 </p>
                                 <p className={showAnswerManufacturer ? '' : 'manufacturer'} >
                                 As a manufacturer, you must be well-versed with the CAR & RPAS Guidance Manual to understand compliance requirements before selling your drone. You will need an IT Act-compliant digital certificate to register yourself.<br></br>
-                                (only for I’m importing a drone) DGFT clearance is needed for importing drones<br />
+                                </p>
+                                <p className={showImport ? '' : 'manufacturer'} >
+                                DGFT clearance is needed for importing drones.<br />
+                                </p>
+                                <p className={showBuildingIndia ? '' : 'manufacturer'} >
                                 Any make of drone you manufacture, assemble or import will need to be NPNT compliant. Digital copies of appropriate compliance certificates are needed to add your make of drone. Once approved, your customers who are registered operators of your make of drone, will be able to generate UINs.
                                 </p>
-
-                                <p className={showAnswerInIndia ? '' : 'in-india'} ><strong>You are purchasing drone in India.</strong> Once you have your license,
-                                    you can apply for “Acquisition of drone” in your dashboard. After aquisition, you can apply for
-                                    a unique identification number (UIN) for your drone.</p>
-                                <p className={showAnswerImport ? '' : 'import'} ><strong>You are importing drone in India.</strong> Once you have your license, you
-                                    can apply for “Acquisition of drone” in your dashboard. After aquisition, you can apply for a
-                                    unique identification number (UIN) for your drone.</p>
-
                                 <Link to="/register" className={selected ? 'button button-light-clean show' : 'button button-light-clean'} >Register</Link>
                             </div>
 
