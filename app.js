25adb8b4aff412e73ab0bd762e27b7d226987197
Sidhant Goel <sidhant@hexcod.in>
2019-06-09 13:35:27 +0530

    serve index.html for everything else

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 
 // Constants
 const PORT = 8000;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.use(express.static('build'))
+app.use(express.static("build"));
+
+// Handle 404
+app.use(function(req, res) {
+  res.sendFile("build/index.html");
+});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
